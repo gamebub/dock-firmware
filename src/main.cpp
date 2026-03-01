00746bfe0b9003bc2f88090009341fb53be0f2aa
Eli Lipsitz <eli.lipsitz@gmail.com>
2026-03-01 09:52:17 -0500

    Clean up some unused stuff

--- src/main.cpp
+++ src/main.cpp
@@ -20,14 +20,6 @@
 
 bi_decl(bi_program_description(GIT_HASH_STRING));
 
-static inline void put_pixel(PIO pio, uint sm, uint32_t pixel_grb) {
-    pio_sm_put_blocking(pio, sm, pixel_grb << 8u);
-}
-
-static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
-    return ((uint32_t)(r) << 8) | ((uint32_t)(g) << 16) | (uint32_t)(b);
-}
-
 int main() {
     stdio_init_all();
     InitUsbDevice();
